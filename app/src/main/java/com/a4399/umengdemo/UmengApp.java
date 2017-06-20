3c025b1b0b2c3e38cd488c924d303ad635fd21b9
jasonkevin88 <305508060@qq.com>
2017-06-20 21:26:51 +0800

    update test

---
+++ app/src/main/java/com/a4399/umengdemo/UmengApp.java
@@ -0,0 +1,15 @@
+package com.a4399.umengdemo;
+
+import android.app.Application;
+
+/**
+ * Created by Chen on 2017/6/20.
+ */
+
+public class UmengApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
